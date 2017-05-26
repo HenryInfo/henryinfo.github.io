841911349668b2f7a82d5592e5f35b60afd3ed58
HenryInfo <johndoe@example.com>
2017-05-25 22:43:16 -0500

    Actualización de plantilla para turismo

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,16 @@
 (function($){
     $(function(){
-
-        $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav()
+        $('.modal').modal();
+        ;
         $('.parallax').parallax();
+        $("#redes").click(function () {
+            $('.tap-target').tapTarget('open');
+
+        })
+        $("#enviarRetro").click(function () {
+            Materialize.toast("Tu mensaje: "+$("#icon_retro").val()+" a sido enviado. Muchas  Gracias!!!", 3000, 'rounded')
+        })
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
--- public/js/siteMaterial.js
+++ public/js/siteMaterial.js
@@ -4,6 +4,17 @@
 
 //Materialize
 $(document).ready(function(){
+    $('.button-collapse').sideNav()
+    $('.modal').modal();
+    ;
+    $('.parallax').parallax();
+    $("#redes").click(function () {
+        $('.tap-target').tapTarget('open');
+    })
+    $("#enviarRetro").click(function () {
+        Materialize.toast("Tu mensaje: "+$("#icon_retro").val()+" a sido enviado. Muchas  Gracias!!!", 3000, 'rounded')
+    })
+
     $('.slider').slider();
     $('.materialboxed').materialbox();
     $(".ui-button .ui-click").click(function(){
